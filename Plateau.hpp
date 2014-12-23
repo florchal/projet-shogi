216a7021ee1951939ace3a0ee3d6f85eea04b5ff
florchal <florian.chalvin@ensea.fr>
2014-12-23 14:30:31 +0100

    Create Plateau.hpp

---
+++ Plateau.hpp
@@ -0,0 +1,29 @@
+#ifndef PLATEAU_HEADER
+#define PLATEAU_HEADER
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector> 
+#include "Pion.hpp"
+
+class Plateau
+{
+	
+	public:
+		Pion* plateau[9][9];
+	private:
+		std::vector<Pion> joueur1;
+		std::vector<Pion> joueur2;
+	
+	public:
+	
+		void setPosition(Pion&,int,int);
+		void capture(Pion&,int,int,int);
+		void parachutage(Pion&,int,int);
+		Pion getPion(int,int);
+		bool hasPion(int,int);
+		Plateau();
+		
+};
+#endif
