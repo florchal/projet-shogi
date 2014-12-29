6eba25ed28c29735aa290c25fda76ada63549f85
Parlepastrop <parlepastrop@gmail.com>
2014-12-29 15:31:12 +0100

    Update Pion.hpp

--- Pion.hpp
+++ Pion.hpp
@@ -11,29 +11,30 @@ enum piece {Case_vide, Soldat, Chariot, Cavalier, Gen_arg, Gen_or, General, Fou,
 
 class Pion {
 	
-	private:
-		int nom;
-		bool promu;
-		int positionx;
-		int positiony;
-		int proprietaire;//1 pour joueur 1, 2 pour joueur 2
-		bool vivant; //true si sur plateau, false sinon
-		
-	public:
-		void setPion(int,int,int,int,bool);
-		void setNom(int);
-		void setPromotion(bool);
-		void setPosition(int,int);
-		void setVivant(bool);
-		void setProprietaire(int);
-		int getNom();
-		bool getPromotion();
-		int getPositionx();
-		int getPositiony();
-		int getId();
-		bool getVivant();
-		int getProprietaire();
-		void retirerPion();
-		Pion(int, int, int, int,bool);
+private:
+	int nom;
+	bool promu;
+	int positionx;
+	int positiony;
+	int proprietaire;//1 pour joueur 1, 2 pour joueur 2
+	bool vivant; //true si sur plateau, false sinon
+	
+public:
+	void setPion(int,int,int,int,bool);
+	void setNom(int);
+	void setPromotion(bool);
+	void setPosition(int,int);
+	void setVivant(bool);
+	void setProprietaire(int);
+	int getNom();
+	bool getPromotion();
+	int getPositionx();
+	int getPositiony();
+	int getId();
+	bool getVivant();
+	int getProprietaire();
+	void retirerPion();
+	Pion(int, int, int, int,bool);
+	//Pion(Pion& pion);
 };
 #endif
