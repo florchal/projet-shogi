ce9e7ae313ca2abd6639581add5faac966c9d322
florchal <florian.chalvin@ensea.fr>
2014-12-31 00:26:50 +0100

    Update Plateau.hpp

--- Plateau.hpp
+++ Plateau.hpp
@@ -10,19 +10,19 @@
 class Plateau
 {
 	
-	public:
-		Pion* plateau[9][9];
-	private:
-		std::vector<Pion> joueur1;
-		std::vector<Pion> joueur2;
+public:
+	Pion* plateau[9][9];
+private:
+	Pion* joueur1[20];
+	Pion* joueur2[20];
 	
-	public:
+public:
 	
-		void setPosition(Pion&,int,int);
-		void capture(Pion&,int,int,int);
-		void parachutage(Pion&,int,int);
-		Pion getPion(int,int);
-		Plateau();
-		
+	void setPosition(Pion&,int,int);
+	void capture(Pion,int,int,int);
+	void parachutage(Pion&,int,int,int);
+	Pion getPion(int,int);
+	Plateau();
+	
 };
 #endif
