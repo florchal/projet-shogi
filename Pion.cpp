195f5492dddef4b6042a15c51303c5be5de583d4
florchal <florian.chalvin@ensea.fr>
2014-12-31 00:25:18 +0100

    Update Pion.cpp
    gestion des promotions

--- Pion.cpp
+++ Pion.cpp
@@ -31,6 +31,70 @@ void Pion::setNom(int nouveau_nom){
 
 void Pion::setPromotion(bool promotion){
 	promu = promotion;
+	if(promotion)
+	{
+		switch(this->getNom())
+		{
+			case Soldat:
+				this->setNom(Soldat_p);
+				break;
+			
+			case Cavalier:
+				this->setNom(Cavalier_p);
+				break;
+				
+			case Gen_arg:
+				this->setNom(Gen_arg_p);
+				break;
+			
+			case Chariot:
+				this->setNom(Chariot_p);
+				break;
+				
+			case Tour:
+				this->setNom(Tour_p);
+				break;
+			
+			case Fou:
+				this->setNom(Fou_p);
+				break;
+				
+			default:
+				break;
+		}
+	}
+	else
+	{
+		switch(this->getNom())
+		{
+			case Soldat_p:
+				this->setNom(Soldat);
+				break;
+			
+			case Cavalier_p:
+				this->setNom(Cavalier);
+				break;
+				
+			case Gen_arg_p:
+				this->setNom(Gen_arg);
+				break;
+			
+			case Chariot_p:
+				this->setNom(Chariot);
+				break;
+				
+			case Tour_p:
+				this->setNom(Tour);
+				break;
+			
+			case Fou_p:
+				this->setNom(Fou);
+				break;
+				
+			default:
+				break;
+		}
+	}
 }
 
 void Pion::setPosition(int x, int y){
@@ -74,4 +138,4 @@ int Pion::getProprietaire(){
 void Pion::retirerPion(){
 	positionx = 100;
 	positiony = 100;
-}
+}	
