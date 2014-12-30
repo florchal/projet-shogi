4b7b1568b3705f5541855f482cdba122704a0ef7
florchal <florian.chalvin@ensea.fr>
2014-12-31 00:26:19 +0100

    Update Plateau.cpp
    parachutage amélioré

--- Plateau.cpp
+++ Plateau.cpp
@@ -12,27 +12,55 @@ Plateau::Plateau(){
 	}
 }
 
-void Plateau::parachutage(Pion& pion, int x_fin, int y_fin)
+void Plateau::parachutage(Pion& pion, int x_fin, int y_fin,int joueur)
 {
-	// TODO retirer le pion de la liste capturée
-	plateau[x_fin][y_fin]=&pion;
+	int i = 0; //supprime le pion de la main du joueur concerné
+	if(joueur==1)
+	{
+		while(joueur1[i]!=&pion)
+		{
+			i++;
+		}
+		joueur1[i]=NULL;
+	}
+	if(joueur==2)
+	{
+		while(joueur2[i]!=&pion)
+		{
+			i++;
+		}
+		joueur2[i]=&pion;
+	}
+	
+	plateau[x_fin][y_fin]=&pion;//met le pion sur le plateau
 }
 
 void Plateau::setPosition(Pion& pion, int x_fin, int y_fin){
 	plateau[x_fin][y_fin]=&pion;
 }
 
-void Plateau::capture(Pion& pion, int x_cap, int y_cap, int preneur){
+void Plateau::capture(Pion pion, int x_cap, int y_cap, int preneur){
+	int i = 0; //met le pion capturé dans la main du joueur concerné
 	if(preneur==1)
 	{
-		joueur1.push_back(pion);
+		while(joueur1[i]!=NULL)
+		{
+			i++;
+		}
+		joueur1[i]=&pion;
 	}
 	if(preneur==2)
 	{
-		joueur2.push_back(pion);
+		while(joueur2[i]!=NULL)
+		{
+			i++;
+		}
+		joueur2[i]=&pion;
 	}
+	plateau[x_cap][y_cap]=NULL; //supprime le pion du plateau
+
 }
-		
+
 Pion Plateau::getPion(int x, int y){
 	return *plateau[x][y];
 }
